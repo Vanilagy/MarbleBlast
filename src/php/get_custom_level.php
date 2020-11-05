79860604b72b8c6d1fe01445106340f530c6377a
Vanilagy <dpayr@musica-viva.de>
2020-11-05 23:33:34 +0100

    Suppress mkdir errors

--- src/php/get_custom_level.php
+++ src/php/get_custom_level.php
@@ -10,7 +10,7 @@ function createPath($path) {
     if (is_dir($path)) return true;
     $prev_path = substr($path, 0, strrpos($path, '/', -2) + 1 );
     $return = createPath($prev_path);
-    return ($return && is_writable($prev_path)) ? mkdir($path) : false;
+    return ($return && is_writable($prev_path)) ? @mkdir($path) : false;
 }
 
 $cwd = getcwd();
--- src/php/upload_wrec.php
+++ src/php/upload_wrec.php
@@ -12,7 +12,7 @@ function createPath($path) {
     if (is_dir($path)) return true;
     $prev_path = substr($path, 0, strrpos($path, '/', -2) + 1 );
     $return = createPath($prev_path);
-    return ($return && is_writable($prev_path)) ? mkdir($path) : false;
+    return ($return && is_writable($prev_path)) ? @mkdir($path) : false;
 }
 createPath($cwd . "/../storage/wrecs"); // Create the wrecs folder
 
