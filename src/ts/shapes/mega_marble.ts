a6cfc26a3df8871535995c5ff702e3c36798af8a
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2021-10-22 23:18:19 +0200

    Fix missing mega marble pick-up name

--- src/ts/shapes/mega_marble.ts
+++ src/ts/shapes/mega_marble.ts
@@ -5,6 +5,7 @@ import { PowerUp } from "./power_up";
 export class MegaMarble extends PowerUp {
 	dtsPath = 'shapes/items/megamarble.dts';
 	sounds = ["pumegamarblevoice.wav", "dosuperjump.wav", "mega_bouncehard1.wav", "mega_bouncehard2.wav", "mega_bouncehard3.wav", "mega_bouncehard4.wav", "mega_roll.wav"];
+	pickUpName = "Mega Marble PowerUp";
 
 	pickUp(): boolean {
 		return this.level.pickUpPowerUp(this);
