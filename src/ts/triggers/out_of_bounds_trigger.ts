b8fc0f466a66952b2f444e352aa4e3f98770d77c
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2022-04-06 18:51:13 +0200

    And that thing

--- src/ts/triggers/out_of_bounds_trigger.ts
+++ src/ts/triggers/out_of_bounds_trigger.ts
@@ -1,9 +1,9 @@
+import { Marble } from "../marble";
 import { Trigger } from "./trigger";
 
 /** An out-of-bounds trigger causes OOB if the marble enters it. */
 export class OutOfBoundsTrigger extends Trigger {
-	onMarbleInside() {
-		this.level.goOutOfBounds();
-		//this.level.replay.recordMarbleInside(this);
+	onMarbleEnter(marble: Marble) {
+		marble.respawn();
 	}
 }
