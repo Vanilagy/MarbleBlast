aef8d02197f34a4531c0ba99be84423d29f673d5
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2022-10-31 00:57:28 +0100

    Update bundler config

--- create_bundle.js
+++ create_bundle.js
@@ -7,7 +7,7 @@ sarcina.bundle({
 	verbose: true,
 	minifyMarkup: false,
 	ignore: ['ts', 'assets'],
-	keep: ['lib/pako.js', 'lib/oggdec.js', 'lib/webm.js', 'manifest.json', 'sw.js'],
+	keep: ['lib/pako.js', 'lib/webm.js', 'manifest.json', 'sw.js'],
 	transpileScript: sarcina.ES2017,
 	insertPolyfill: true,
 	handleInlineScript: false
